3f4f9f511684c1952337d986c741c9cee25acdec
gd-harco <gd-harco@student.42lyon.fr>
2022-11-08 13:11:13 +0100

    correction de memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,27 +6,26 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 16:02:52 by gd-harco          #+#    #+#             */
-/*   Updated: 2022/11/07 17:30:08 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2022/11/08 13:05:53 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	x;
 	char	*str;
 
-
 	if (src < dst)
 	{
 		x = len - 1;
-		while (x >= 0)
+		while (x > 0)
 		{
 			((char *)dst)[x] = ((char *)src)[x];
 			x--;
 		}
+		((char *)dst)[x] = ((char *)src)[x];
 		return (dst);
 	}
 	else
