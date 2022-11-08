c36254b06673f9362bc972be23de4664d54b5f56
gd-harco <gd-harco@student.42lyon.fr>
2022-11-08 16:52:54 +0100

    correction calloc pour gerer les overflows

--- fonc_libc/ft_calloc.c
+++ fonc_libc/ft_calloc.c
@@ -6,20 +6,34 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 11:50:45 by gd-harco          #+#    #+#             */
-/*   Updated: 2022/11/07 14:11:56 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2022/11/08 16:51:54 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
+size_t static	multiply(size_t Count, size_t Size)
+{
+	size_t	result;
+
+	result = Count * Size;
+	if (Size != Count / result)
+		return (NULL);
+	return (Size);
+}
+
 void	*ft_calloc( size_t elementCount, size_t elementSize )
 {
 	void	*result;
+	size_t	malloc_size;
 
-	result = malloc(elementSize * elementCount);
+	malloc_size = multiply(elementCount, elementSize);
+	if (malloc_size == NULL)
+		return (NULL);
+	result = malloc(malloc_size);
 	if (result == NULL)
 		return (NULL);
-	ft_bzero(result, elementCount * elementSize);
+	ft_bzero(result, malloc_size);
 	return (result);
 }
