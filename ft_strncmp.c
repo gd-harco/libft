04e83ba1dfe6b0835fc33915783048fed1b60301
gd-harco <gd-harco@student.42lyon.fr>
2022-11-11 13:00:55 +0100

    ajout strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,10 +6,24 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 12:30:19 by gd-harco          #+#    #+#             */
-/*   Updated: 2022/11/11 12:39:16 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2022/11/11 12:46:43 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int	x;
+
+	x = 0;
+	while ((s1[x] || s2[x]) && x <= n)
+	{
+		if (s1[x] == s2[x])
+			x++;
+		else
+			return (s1[x] - s2[x]);
+	}
+	return (0);
+
+}
