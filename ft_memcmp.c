d4a0919fd436d64a2f0dc92a7ee9fb887a108ab8
gd-harco <gd-harco@student.42lyon.fr>
2022-11-14 13:41:11 +0100

    bugfixes, la fonction ne doit pas s'arreter lorsqu'elle tombe sur un \0

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 14:07:51 by gd-harco          #+#    #+#             */
-/*   Updated: 2022/11/11 14:12:33 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2022/11/14 13:40:06 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	int	x;
 
 	x = 0;
-	while ((((unsigned char *)s1)[x] || ((unsigned char *)s2)[x]) && n > 0)
+	while (n > 0)
 	{
 		if (((unsigned char *)s1)[x] == ((unsigned char *)s2)[x])
 		{
