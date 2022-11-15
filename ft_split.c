4097eb9c39669bbfd633ca95c6a81dcbdf4b26ef
gd-harco <gd-harco@student.42lyon.fr>
2022-11-15 14:27:24 +0100

    commit en urgence

--- ft_split.c
+++ ft_split.c
@@ -12,6 +12,25 @@
 
 #include "libft.h"
 
+static void	free_all(int i, char **dest)
+{
+	while (i <= 0)
+	{
+		free(dest[i]);
+		i--;
+	}
+}
+
+static int	get_word_length(const char *s, char charset)
+{
+	int	x;
+
+	x = 0;
+	while (!ft_strchr(&s[x], charset))
+		x++;
+	return (x);
+}
+
 static int	get_nb_word(char const *s, char c)
 {
 	int	nb_word;
@@ -30,13 +49,6 @@ static int	get_nb_word(char const *s, char c)
 	return (nb_word);
 }
 
-static int	in_charset(char c, char charset)
-{
-	if (c == charset)
-		return (1);
-	return (0);
-}
-
 static void	splitnwrite(char **dest, const char *str, char charset, int nbr)
 {
 	int		i;
@@ -44,36 +56,45 @@ static void	splitnwrite(char **dest, const char *str, char charset, int nbr)
 	int		len;
 	char	*temp;
 
-	i = 0;
-	while (i < nbr)
+	i = -1;
+	while (i++ < nbr)
 	{
-		while (in_charset(*str, charset))
-			str++;
-		if (!in_charset(*str, charset))
+		if (!ft_strchr(str, charset))
 		{
-			len = ft_strlen(str);
+			len = get_word_length(str, charset);
 			temp = malloc(sizeof(char) * (len + 1));
+			if (!temp)
+				return (free_all(i, dest));
 			j = 0;
-			while (j < len)
+			while (!ft_strchr(str, charset))
 				temp[j++] = *str++;
 			temp[j] = '\0';
 			dest[i] = temp;
-			free(temp);
 		}
-		i++;
 	}
+	free (temp);
 	dest[i] = NULL;
 }
 
 char	**ft_split(char const *s, char c)
 {
 	int		nb_word;
-	char	**big_table;
+	char	**dest;
 
+	if (!c)
+		return (malloc(sizeof(void *)));
 	nb_word = get_nb_word(s, c);
-	big_table = malloc(sizeof(char *) * (nb_word + 1));
-	if (big_table == NULL)
+	if (nb_word == 0)
 		return (NULL);
-	splitnwrite(big_table, s, c, nb_word);
-	return (big_table);
+	dest = malloc(sizeof(char *) * (nb_word + 1));
+	if (dest == NULL)
+		return (NULL);
+	splitnwrite(dest, s, c, nb_word);
+	return (dest);
+}
+void main()
+{
+char *string = "      split       this for   me  !       ";
+	char **expected = ((char*[6]){"split", "this", "for", "me", "!", ((void *)0)});
+	char **result = ft_split(string, ' ');
 }
