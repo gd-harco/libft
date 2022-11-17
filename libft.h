28834b7498e291ddf67e7560447498f8970286de
gd-harco <gd-harco@student.42lyon.fr>
2022-11-17 12:15:37 +0100

    ajout header dans makefile, preparation du .h en vu de la realisation des bonus

--- libft.h
+++ libft.h
@@ -15,6 +15,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t elementCount, size_t elementSize);
