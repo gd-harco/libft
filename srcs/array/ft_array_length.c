ba7b8745e4c67a7cfc787c8b52eb833c21759916
gd-harco <gd-harco@student.42lyon.fr>
2022-12-15 16:55:18 +0100

    mise a la norme

--- srcs/array/ft_array_length.c
+++ srcs/array/ft_array_length.c
@@ -6,14 +6,14 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 21:38:25 by gd-harco          #+#    #+#             */
-/*   Updated: 2022/12/12 19:44:03 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2022/12/15 16:54:55 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
- size_t	array_length(void **array)
- {
+size_t	array_length(void **array)
+{
 	size_t	x;
 
 	if (!array)
@@ -22,4 +22,4 @@
 	while (array[x])
 		x++;
 	return (x);
- }
+}
