a484e4b64371bbf74d7fa60fd22dd53097d7fc00
gd-harco <gd-harco@student.42lyon.fr>
2023-03-28 12:47:09 +0200

    fixed ft_dprintf, header declaration was wrong

--- includes/io.h
+++ includes/io.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   io.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gd-harco <gd-harco@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 18:29:44 by gd-harco          #+#    #+#             */
-/*   Updated: 2023/03/15 18:29:44 by gd-harco         ###   ########.fr       */
+/*   Updated: 2023/03/28 12:46:38 by gd-harco         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <unistd.h>
 
 int		ft_printf(const char *txt, ...);
-int		ft_dprintf(int fd, const char *txt, ...);
+int		ft_dprintf(int fd, char *txt, ...);
 size_t	ft_putchar_fd(char c, int fd);
 size_t	ft_putendl_fd(char *s, int fd);
 size_t	ft_putnbr_fd(int n, int fd, size_t size);
