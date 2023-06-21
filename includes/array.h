5b3c23f03ebdef65170cc7b1c326555cf1e19747
gd-harco <gd-harco@student.42lyon.fr>
2023-06-21 13:24:16 +0200

    added ft_add/remove_arrat_line.c

--- includes/array.h
+++ includes/array.h
@@ -20,4 +20,7 @@ void	**ft_array_dup(void **array, bool free_array, bool dup_strings);
 char	**ft_merge_array(char **p_array, char **s_array,
 			bool free_array, bool free_line);
 void	ft_sort_array(char **array);
+void	**ft_add_array_line(void **array, void *content);
+void	ft_remove_array_line(void **array, size_t index, bool free_line);
+
 #endif
---
+++ srcs/array/ft_add_array_line.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_add_array_line.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 13:21:17 by gd-harco          #+#    #+#             */
+/*   Updated: 2023/06/21 13:21:21 by gd-harco         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "array.h"
+#include "memory.h"
+/**
+ * @brief Create a duplicate of an array with an additional pointer at the end.
+ * @param array The array in which to add the pointer.
+ * @param content The pointer to add.
+ * @return The new array with the pointer added.
+ */
+void	**ft_add_array_line(void **array, void *content)
+{
+	void	**new_array;
+
+	new_array = ft_calloc(ft_array_length(array) + 2, sizeof(void *));
+	if (!new_array)
+		return (NULL);
+	ft_memmove(new_array, array, ft_array_length(array) * sizeof(void *));
+	new_array[ft_array_length(array)] = content;
+	free(array);
+	return (new_array);
+}
