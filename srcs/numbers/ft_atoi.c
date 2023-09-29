150bd542af5c23685a9f12a682fec0c4510f4b5a
gd-harco <gd-harco@student.42lyon.fr>
2023-09-29 15:39:01 +0200

    updated atoi to reflect change made in Cub

--- srcs/numbers/ft_atoi.c
+++ srcs/numbers/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: gd-harco <gd-harco@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 15:41:46 by gd-harco          #+#    #+#             */
-/*   Updated: 2022/11/09 16:22:01 by gd-harco         ###   ########lyon.fr   */
+/*   Updated: 2023/09/28 15:48:22 by gd-harco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,29 @@ static int	ft_overflow(int sign)
 		return ((int)LONG_MAX);
 }
 
+int	get_sign(const char **str)
+{
+	if (**str == '+' || **str == '-')
+	{
+		if (**str == '-')
+		{
+			(*str)++;
+			return (-1);
+		}
+		(*str)++;
+	}
+	return (1);
+}
+
 int	ft_atoi(const char *str)
 {
 	long	result;
 	int		sign;
 
 	result = 0;
-	sign = 1;
 	while (*str == ' ' || (*str >= 9 && *str <= 13))
 		str++;
-	if (*str == '+' || *str == '-')
-	{
-		if (*str == '-')
-			sign = -1;
-		str++;
-	}
+	sign = get_sign(&str);
 	if (!ft_isdigit(*str))
 		return (errno = 20, 0);
 	while (ft_isdigit(*str))
@@ -44,7 +52,11 @@ int	ft_atoi(const char *str)
 		result = (result * 10) + (*str - '0');
 		str++;
 	}
-	if (*str != '\0')
-		return (errno = 20, 0);
+	while (*str != '\0')
+	{
+		if (!ft_isspace(*str))
+			return (errno = 20, 0);
+		str++;
+	}
 	return ((int)result * sign);
 }
